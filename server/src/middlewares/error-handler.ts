92923f319a21e0bbb753d4b40e3f746b2d012752
Anioke Sebastian <aniokechukwudi7@gmail.com>
2026-03-23 22:38:07 +0100

    refactor: remove server code and related files for a clean slate

--- server/src/middlewares/error-handler.ts
+++
@@ -1,9 +0,0 @@
-import type { ErrorRequestHandler } from 'express';
-
-export const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
-  const message = error instanceof Error ? error.message : 'Unexpected server error';
-  res.status(500).json({
-    success: false,
-    message
-  });
-};
--- server/src/server.ts
+++
@@ -1,7 +0,0 @@
-import app from './app.js';
-import { envConfig } from './config.js';
-import { logger } from './utils/logger.js';
-
-app.listen(envConfig.PORT, () => {
-  logger.info(`Lauki Connect server running on port ${envConfig.PORT}`);
-});
